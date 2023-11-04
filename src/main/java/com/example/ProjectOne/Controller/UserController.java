e6de9afd6d7ccb133157e09d0ed975494d40e51d
PavanKalyan <pavankalyan6md@gmail.com>
2023-11-04 13:55:09 +0530

    commit

--- src/main/java/com/example/ProjectOne/Controller/UserController.java
+++ src/main/java/com/example/ProjectOne/Controller/UserController.java
@@ -3,10 +3,10 @@ package com.example.ProjectOne.Controller;
 import com.example.ProjectOne.Entity.User;
 import com.example.ProjectOne.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fb-api")
@@ -28,5 +28,28 @@ public class UserController {
     public void createUser(@RequestBody User user) {
         // Call the insertNewUser() method from the UserService to create a new user
         service.insertNewUser(user);
+    }
+
+    // Define an HTTP GET endpoint for retrieving user details by ID
+    @GetMapping("/getUserDetailsById/{id}")
+    public Optional<User> getDetailsById(@PathVariable int id) {
+        // Call the getUserById() method from the UserService to retrieve user details by ID
+        return service.getUserById(id);
+    }
+
+    // Define an HTTP DELETE endpoint for deleting a user by ID
+    @DeleteMapping("/deleteUserById/{id}")
+    public void deleteUserById(@PathVariable int id) {
+        // Call the deleteUserDetailsById() method from the UserService to delete a user by ID
+        service.deleteUserDetailsById(id);
+    }
+
+    // Define an HTTP PUT endpoint for updating existing user details by ID
+    @PutMapping("/updateUserById/{id}")
+    public void updateUserById(@RequestBody User updatedUser, @PathVariable int id) {
+        // Call the getDetailsById() method to retrieve the old user by ID
+        Optional<User> oldUser = getDetailsById(id);
+        // Call the updateUserDetailsById() method from the UserService to update user details
+        service.updateUserDetailsById(oldUser, updatedUser);
     }
 }
--- src/main/java/com/example/ProjectOne/Service/UserService.java
+++ src/main/java/com/example/ProjectOne/Service/UserService.java
@@ -5,6 +5,7 @@ import com.example.ProjectOne.Repository.UserJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,5 +24,29 @@ public class UserService {
     public void insertNewUser(User user) {
         // Call the save() method from UserJpaRepository to save the new user to the database
         userJpaRepository.save(user);
+    }
+
+    // Method to retrieve a user by their ID
+    public Optional<User> getUserById(int id) {
+        return userJpaRepository.findById(id);
+    }
+
+    // Method to delete a user by their ID
+    public void deleteUserDetailsById(int id) {
+        userJpaRepository.deleteById(id);
+    }
+
+    // Method to update user details by ID
+    public void updateUserDetailsById(Optional<User> old_user, User updated_user) {
+        if (old_user.isPresent()) {
+            // If the old user is found, update its properties with the new values
+            User user = old_user.get();
+            user.setUserId(updated_user.getUserId());
+            user.setUserEmail(updated_user.getUserEmail());
+            user.setUsername(updated_user.getUsername());
+
+            // Save the updated user to the database
+            userJpaRepository.save(user);
+        }
     }
 }
